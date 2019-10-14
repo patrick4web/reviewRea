51f1637eb19d8f59f8943beccc920baa922c0f24
patrick4web <pat-wong@hotmail.com>
2019-10-14 15:05:23 -0700

    Setup completed

---
+++ comp/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View} from 'react-native';
+
+
+
+
+function Main(){
+    return(
+        <View>
+
+           
+        </View>
+    )
+}
+
+export default Main;
