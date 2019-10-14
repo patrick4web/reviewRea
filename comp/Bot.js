9da755ef2775c5bd55ee958bdd972cb96862ace5
LucaMattia <lucamat1997@gmail.com>
2019-10-14 15:42:26 -0700

    Started with the process

---
+++ comp/Bot.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
+import styles from '../styles/Bot-styles';
+
+function Bot(){
+    
+    
+    
+    return(
+        <View>
+
+            <TextInput />
+        </View>
+    );
+}
+
+
+export default Bot;
